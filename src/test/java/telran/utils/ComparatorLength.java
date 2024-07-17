a77eab8cd838d12f573db74603e278c73d64716e
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-17 21:28:02 +0300

    Added custom Comparators (2 was written at the class (CW#7) and the third one (COmparatorInteger) is the part of HW#7

---
+++ src/test/java/telran/utils/ComparatorLength.java
@@ -0,0 +1,12 @@
+package telran.utils;
+
+import java.util.Comparator;
+
+public class ComparatorLength implements Comparator<String>
+{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2.length();
+    }
+}
