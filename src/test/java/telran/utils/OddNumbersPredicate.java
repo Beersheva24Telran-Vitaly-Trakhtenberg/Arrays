119878743e0761e722c85a07182c5c45912acaf3
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-22 15:08:55 +0300

    Added new test class (CW#8 - Predicate)

---
+++ src/test/java/telran/utils/OddNumbersPredicate.java
@@ -0,0 +1,11 @@
+package telran.utils;
+
+import java.util.function.Predicate;
+
+public class OddNumbersPredicate implements Predicate<Integer>
+{
+    @Override
+    public boolean test(Integer arg0) {
+        return arg0 % 2 != 0;
+    }
+}
