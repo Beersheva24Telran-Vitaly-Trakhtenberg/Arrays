2838788914779fbd1033f6d9a7c59c0a1e9997fa
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-22 21:47:27 +0300

    HW#8 predicate for removeIfTest

---
+++ src/test/java/telran/utils/IsPositiveEvenPredicate.java
@@ -0,0 +1,11 @@
+package telran.utils;
+
+import java.util.function.Predicate;
+
+public class IsPositiveEvenPredicate implements Predicate<Integer>
+{
+    @Override
+    public boolean test(Integer arg0) {
+        return arg0 % 2 == 0 && arg0 > 0;
+    }
+}
