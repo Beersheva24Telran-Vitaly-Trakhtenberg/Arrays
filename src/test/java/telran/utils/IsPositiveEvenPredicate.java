cf80358d060acd6754d2eb9b9205b923e226e61d
Vitaly Trakhtenberg <webdev@tca.by>
2024-07-23 15:30:23 +0300

    method 'test' changed: refactored predicate (didn't works with negative selection)

--- src/test/java/telran/utils/IsPositiveEvenPredicate.java
+++ src/test/java/telran/utils/IsPositiveEvenPredicate.java
@@ -6,6 +6,7 @@ public class IsPositiveEvenPredicate implements Predicate<Integer>
 {
     @Override
     public boolean test(Integer arg0) {
-        return arg0 % 2 == 0 && arg0 > 0;
+        boolean res = arg0 % 2 == 0 || arg0 < 0;
+        return arg0 % 2 == 0 || arg0 < 0;
     }
 }
